ae475043b37bdfdfeef8de8eb87913767643c9bf
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2025-02-08 16:58:48 +0600

    Update

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,14 +1,14 @@
-export const MONGODB_URL= "mongodb+srv://roots:roots@cluster0.3pcbx.mongodb.net/assignments_modules_26"
+export const MONGODB_URL ="mongodb+srv://FawjulDemo:0arOjL584U44GvK3@cluster0.cymiz.mongodb.net/Profolio";
 
 //ACCESS TOKEN
-export const JWT_SECRET_TOKEN= "123RohanAzim"
-export const JWT_EXPIRATION_TIME_TOKEN = "5h"
+export const JWT_SECRET_TOKEN = "123RohanAzim";
+export const JWT_EXPIRATION_TIME_TOKEN = "5h";
 
-export const Max_JSON_SIZE = "50mb"
-export const URL_ENCODER = true
+export const Max_JSON_SIZE = "50mb";
+export const URL_ENCODER = true;
 
-export const REQUEST_LIMIT_TIME = 15 * 60 * 1000
-export const REQUEST_LIMIT_NUMBER = 9999999999
+export const REQUEST_LIMIT_TIME = 15 * 60 * 1000;
+export const REQUEST_LIMIT_NUMBER = 9999999999;
 
-export const WEB_CACHE = false
-export const PORT = 4000
+export const WEB_CACHE = false;
+export const PORT = 4000;
